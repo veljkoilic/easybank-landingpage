628ead70878573fe3e3993229692b1d0d27fcc2a
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-14 14:14:52 +0200

    added in components to be rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Intro } from "./components/Intro";
+import { LatestArticles } from "./components/LatestArticles";
+import { Why } from "./components/Why";
 
 function App() {
   return (
     <main className="App">
       <Header/>
+      <Intro/>
+      <Why/>
+      <LatestArticles />
+      <Footer/>
     </main>
   );
 }
